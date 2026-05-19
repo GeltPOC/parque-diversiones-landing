cc75ee3b5acbcb9e836dde020431c0e77b7e0daf
GeltTech <tech@gelt.com>
2026-05-19 11:00:23 +0200

    feat: src/features/landing/components/LandingPage.tsx

---
+++ src/features/landing/components/LandingPage.tsx
@@ -0,0 +1,32 @@
+'use client'
+
+import { useState } from 'react'
+import Navbar from './Navbar'
+import HeroSection from './HeroSection'
+import AttractionsSection from './AttractionsSection'
+import PricingSection from './PricingSection'
+import GallerySection from './GallerySection'
+import ScheduleSection from './ScheduleSection'
+import LocationSection from './LocationSection'
+import CtaSection from './CtaSection'
+import Footer from './Footer'
+import TicketModal from './TicketModal'
+
+export default function LandingPage() {
+  const [showModal, setShowModal] = useState(false)
+
+  return (
+    <div className="min-h-screen">
+      <Navbar onBuyTicket={() => setShowModal(true)} />
+      <HeroSection onBuyTicket={() => setShowModal(true)} />
+      <AttractionsSection />
+      <PricingSection onBuyTicket={() => setShowModal(true)} />
+      <GallerySection />
+      <ScheduleSection />
+      <LocationSection />
+      <CtaSection onBuyTicket={() => setShowModal(true)} />
+      <Footer />
+      {showModal && <TicketModal onClose={() => setShowModal(false)} />}
+    </div>
+  )
+}
